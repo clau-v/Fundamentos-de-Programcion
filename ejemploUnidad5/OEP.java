b4606fcac3bc0bd9088e6c4977992baa3cc48b87
Claudia Vargas <clau.99v@gmail.com>
2020-10-26 00:37:27 -0400

    ejemplo2 de la unidad 5

--- ejemploUnidad5/OEP.java
+++ ejemploUnidad5/OEP.java
@@ -1,5 +1,5 @@
 public class OEP{
-    public String Verificar(Persona persona){
+    public String verificar(Persona persona){
         String res;
         int edadPersona=persona.getEdad();
         String nombrePersona=persona.getNombre();
